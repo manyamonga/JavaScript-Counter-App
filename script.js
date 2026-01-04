bb66f193b6aae2fab1f5e1948186149b4482e64d
Manya Monga <manyamonga6@gmail.com>
2026-01-04 18:15:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const num = document.querySelector("#number");
+const add = document.querySelector("#addBtn");
+const subtract = document.querySelector("#subtractBtn");
+const reset = document.querySelector("#resetBtn");
+ 
+function getRandomNumber() {
+    return Math.ceil(Math.random() * 100) + 1;
+}
+let count = getRandomNumber();
+num.textContent = count;
+
+add.addEventListener("click" , function() {
+    count++;
+    num.textContent = count;
+});
+
+subtract.addEventListener("click" , function() {
+    count--;
+    num.textContent = count;
+});
+
+reset.addEventListener("click" , function() {
+    count = 0;
+    num.textContent = count;
+});
+
+if (count > 50) {
+    num.style.color = "green";
+} else if (count <= 50) {
+    num.style.color = "orange";
+} 
